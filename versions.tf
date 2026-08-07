5c576be39a2ddef577968cd91a71a6aae462236e
kbikas <kostasbikas02@gmail.com>
2026-08-07 16:34:28 +0300

    added tf providers

---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+
+  required_version = ">= 1.2"
+
+}
